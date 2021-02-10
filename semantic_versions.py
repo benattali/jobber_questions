e9331e040e24ab4aaae350bb337eefa965598b23
Ben Attali <ben.attali8@gmail.com>
2021-02-09 20:21:20 -0500

    added more test cases + comments

--- semantic_versions.py
+++ semantic_versions.py
@@ -2,22 +2,30 @@ import unittest
 
 
 def nextVersion(version_num: str) -> str:
-    if len(version_num) < 1:
-        raise ValueError("An empty string was passed.")
+    # iterate through the string in reverse order
+    ## if char is a period
+    ### add it to temp_str
+    ## if loop reaches the last element (i.e the first number)
+    ### increment that digit by 1 and add it to new_version
+    ## if char is 9
+    ### add 0 to temp_str
+    ## if char is not 9
+    ### increment that digit by 1 and add it to new_version and break from the loop
 
     temp_str = ""
     new_version = ""
+
     for i, char in enumerate(reversed(version_num)):
         if char == ".":
             temp_str += char
         elif i == len(version_num) - 1:
-            temp = int(char) + 1
-            new_version += version_num[: -1 - i] + str(temp) + temp_str[::-1]
+            temp_num = int(char) + 1
+            new_version += version_num[: -1 - i] + str(temp_num) + temp_str[::-1]
         elif int(char) == 9:
             temp_str += "0"
         else:
-            temp = int(char) + 1
-            new_version += version_num[: -1 - i] + str(temp) + temp_str[::-1]
+            temp_num = int(char) + 1
+            new_version += version_num[: -1 - i] + str(temp_num) + temp_str[::-1]
             break
 
     return new_version
@@ -42,9 +50,24 @@ class Test(unittest.TestCase):
         expected = "10.0"
         self.assertEqual(actual, expected)
 
-    def test_version_ends_with_nine(self):
+    def test_version_ends_with_nine_no_decimal(self):
         actual = nextVersion("99")
         expected = "100"
+        self.assertEqual(actual, expected)
+
+    def test_version_starts_with_9_digit_ends_with_0(self):
+        actual = nextVersion("99.0")
+        expected = "99.1"
+        self.assertEqual(actual, expected)
+
+    def test_version_starts_with_9_digit_ends_with_9(self):
+        actual = nextVersion("99.0.9")
+        expected = "99.1.0"
+        self.assertEqual(actual, expected)
+
+    def test_version_starts_with_9_digit_ends_with_two_0s(self):
+        actual = nextVersion("99.0.0")
+        expected = "99.0.1"
         self.assertEqual(actual, expected)
 
     def test_version_ends_with_multiple_nines(self):
@@ -52,9 +75,35 @@ class Test(unittest.TestCase):
         expected = "1.0.0"
         self.assertEqual(actual, expected)
 
-    def test_error_with_empty_string(self):
-        with self.assertRaises(ValueError):
-            nextVersion("")
+    def test_version_has_multiple_numbers_starts_with_0_ends_with_nine(self):
+        actual = nextVersion("0.8.9")
+        expected = "0.9.0"
+        self.assertEqual(actual, expected)
+
+    def test_version_has_multiple_numbers_starts_with_0(self):
+        actual = nextVersion("0.8.1")
+        expected = "0.8.2"
+        self.assertEqual(actual, expected)
+
+    def test_version_large_number(self):
+        actual = nextVersion("10001")
+        expected = "10002"
+        self.assertEqual(actual, expected)
+
+    def test_version_large_number_ends_with_9(self):
+        actual = nextVersion("10009")
+        expected = "10010"
+        self.assertEqual(actual, expected)
+
+    def test_version_large_number_ends_with_9_decimal_ends_with_9(self):
+        actual = nextVersion("10009.9")
+        expected = "10010.0"
+        self.assertEqual(actual, expected)
+
+    def test_version_chain_the_9s(self):
+        actual = nextVersion("9.9.9.9.9.9.9.9.9.9.9.9.9")
+        expected = "10.0.0.0.0.0.0.0.0.0.0.0.0"
+        self.assertEqual(actual, expected)
 
 
 unittest.main(verbosity=2)
